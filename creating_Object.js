45ce286f24d20205152b9cfe69098a6164bb6b9e
Faisal Ahmmed <ahmmedfaisal586@gmail.com>
2025-05-14 15:07:00 +0600

    Create creating_Object.js

---
+++ creating_Object.js
@@ -0,0 +1,21 @@
+const Person  ={};   
+
+Person.firstName="Faisal";
+Person.lastName="Ahmmed";
+Person.age=25;
+Person.eyeColor="brown";
+
+
+Person.profession="software Developer";
+
+console.log(Person);
+
+const Person2 = new Object();
+
+Person2.firstName="Jannatul";
+Person2.lastname="Ferdous";
+
+Person2.age=24;
+Person2.eyeColor="blue";
+
+console.log(Person2);
